09f5ffa81a15cdae143d49ebb85767007403b558
shraysingalcs27 <shray.singal.cs27@iilm.edu>
2026-02-23 16:00:03 +0530

    Create Reverse Linked List.py

---
+++ VS Code/Reverse Linked List.py
@@ -0,0 +1,17 @@
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = None
+        while head:
+            temp = head
+            head = head.next
+
+            temp.next = p
+            p = temp
+        return p
