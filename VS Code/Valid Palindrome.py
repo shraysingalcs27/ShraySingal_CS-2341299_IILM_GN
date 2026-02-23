5448402bc00a3986fcd956ab26179961314a5f0a
shraysingalcs27 <shray.singal.cs27@iilm.edu>
2026-02-23 15:51:48 +0530

    Create  Valid Palindrome.py

---
+++ VS Code/Valid Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start = 0
+        end = len(s) - 1
+        while(start <= end):
+            while start <= end and  (not s[start].isalnum()):
+                start = start + 1
+            while start <= end and (not s[end].isalnum()):
+                end = end - 1
+            if start <= end and  s[start].lower() != s[end].lower():
+                return False
+            start = start + 1
+            end = end - 1
+        else :
+                return True
